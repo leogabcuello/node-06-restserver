e141831a1df93d64a3d48f5e4032e10d7865397b
Leonardo <leogabcuello@gmail.com>
2021-05-13 00:49:37 -0300

    swagger documentation

--- src/loaders/server/expressServer.js
+++ src/loaders/server/expressServer.js
@@ -54,6 +54,10 @@ class ExpressServer {
     _errorHandler() {
         this.app.use((err, req, res, next) => {
             const code = err.code || 500;
+            
+            logger.error(`${code} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+            logger.error(err.stack);
+
             res.status(code);
             const body = {
                 error: {
